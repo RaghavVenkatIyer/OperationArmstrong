7ab1e244581d1aa959471d2305ede33eaf75e420
RaghavVenkatIyer <161187786+RaghavVenkatIyer@users.noreply.github.com>
2025-12-09 10:13:01 +0530

    Update blackholesimulationoriginal.py

--- blackholesimulationoriginal.py
+++ blackholesimulationoriginal.py
@@ -29,24 +29,13 @@ print(orbitalvelocity, "is the orbital velocity of an object")
 gravitationalredshift = (1 / math.sqrt(1 - (r_s / r))) - 1
 print(gravitationalredshift, "is the gravitational redshift")
     
-print('Compressed Length Calculator Machine')
-lengthselection=int(input('Enter 0 if providing viewers length, enter 1 if providing fallers length'))
-if lengthselection==0:
-    viewersleng=float(input('Enter the viewers length '))
-    fallerslength=viewersleng*math.sqrt(1-(r_s/r))
-    print(fallerslength,'is the fallerslength')
-else:
-    fallerslen=float(input('Enter the fallers length'))
-    viewerslength=fallerslen/(math.sqrt(1-(r_s/r)))
-    print(viewerslength,'is the viewers length')
-  
 print('Gravitational Potential Energy Engine')
 m=int(input('Enter the mass of the object near the black hole'))
 gravitationalpotentialenergy=-((G*m*M)/r)
 print(gravitationalpotentialenergy,'is the gravitational potential energy')
 
 print('Acceleration required to hover at distance r from black hole calculator')
-acceleration=G*M/r**2*math.sqrt(1-(r_s/r))
+acceleration=(G*M)/(r**2*math.sqrt(1-r_s/r))
 print(acceleration, 'is the acceleration required to hover above the black hole')
 
 print('Orbital period near a black hole calculator')
@@ -58,6 +47,27 @@ h=int(input('Enter the persons height'))
 tidalforce=(2*G*M*h)/(r**3)
 print(tidalforce, 'is the tidalforce')
 
+print('Usage of Special Relativistic Formulae')
+print('Accretion Disk Physics Calculator')
+print('Time Dilation for orbiting particle')
+dtau_dt = math.sqrt(1 - 3*G*M/(r*c**2))
+print(dtau_dt, "is the SR time dilation for an orbiting particle")
+
+print('Local Orbital Velocity Calculator')
+omega = math.sqrt(G*M / r**3)
+v_local = (r * omega) / math.sqrt(1 - r_s/r)
+print(v_local, "is the local orbital velocity seen by a stationary observer")
+
+print('Lorentz Factor Calculation')
+gamma = 1 / math.sqrt(1 - (v_local**2 / c**2))
+print(gamma, "is the SR Lorentz factor of the orbiting particle")
+
+print('Full observed frequency shift')
+theta=int(input('Enter the angle between the velocity and observer line of sight'))
+theta=math.radians(theta)
+g_factor = math.sqrt(1 - r_s/r) / (gamma * (1 - (v_local/c) * math.cos(theta)))
+print(g_factor, "is the total relativistic redshift factor")
+
 print('All calculations are printed and are successful, below is the summary of results : ')
 print(r_s,'is the schwarzchild radius')
 print(escapevelocity,'is the escape velocity')
@@ -68,6 +78,10 @@ print(gravitationalpotentialenergy, 'is the gravitational potential energy')
 print(acceleration,'is the acceleration')
 print(orbitalperiod,'is the orbital period')
 print(tidalforce,'is the tidal force')
+print(g_factor,'is the total relativistic redshift factor')
+print(gamma,'is the special relativistic time lorentz factor of an orbiting particle')
+print(v_local, "is the local orbital velocity seen by a stationary observer")
+print(dtau_dt, "is the GR time dilation for an orbiting particle")
 k=str(input(''))
 if k =='kill':
       print('This code has been terminated')
